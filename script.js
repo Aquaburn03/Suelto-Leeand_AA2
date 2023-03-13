f08bc88427be64205f4d194862c40b7f48ab0761
Aquaburn03 <127409350+Aquaburn03@users.noreply.github.com>
2023-03-13 14:33:51 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,12 @@
     let i = 1;
     let whileLoopResult = '';
     while (i <= 51) {
-      whileLoopResult += i + ' ';
-      i+=2;
+      if(i<51){
+      	whileLoopResult += i + ', ';
+  } else{
+  	whileLoopResult += i + ' ';
+  }
+  i+=2;
     }
     document.getElementById('while-loop').innerHTML = whileLoopResult;
 
@@ -11,8 +15,12 @@
     let j = 2;
     let doWhileLoopResult = '';
     do {
-      doWhileLoopResult += j + ' ';
-      j += 2;
+  	if (j<50){
+  		doWhileLoopResult += j + ', ';
+  	}else{
+  		doWhileLoopResult += j + ' ';
+  }
+  j+=2;
     } while (j <= 50);
     document.getElementById('do-while-loop').innerHTML = doWhileLoopResult;
 
@@ -20,7 +28,11 @@
     let k = 1;
     let forLoopResult = '';
     for (let n = 0; k <= 4096; n++) {
-      forLoopResult += k + ' ';
+      if (k<4096){
+      	forLoopResult += k + ', ';
+      }else{
+      	forLoopResult += k + ' ';
+      }
       k *= 2;
     }
     document.getElementById('for-loop').textContent = forLoopResult;
